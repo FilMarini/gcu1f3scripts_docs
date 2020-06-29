34b758ee9a7842467068d53116df96de00d61b55
Filippo Marini <filippo.marini@pd.infn.it>
2020-06-29 14:33:27 +0200

    fix bug in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ author = 'Filippo Marini'
 # The full version, including alpha/beta/rc tags
 release = '1.0'
 
+# The master toctree document.
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
